e04b12f56dae74ba2b5c464e93b25ca83116c177
Mauro <mhdelmonde@gmail.com>
2024-03-31 16:24:29 -0400

    Add a controller

--- obj/Debug/net8.0/MvcMovie.AssemblyInfo.cs
+++ obj/Debug/net8.0/MvcMovie.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("MvcMovie")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+318cb02e5abf56d3c48cb8e55a65cc64564ed569")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6b8aea665e4d9494f5c503791a5193932b311502")]
 [assembly: System.Reflection.AssemblyProductAttribute("MvcMovie")]
 [assembly: System.Reflection.AssemblyTitleAttribute("MvcMovie")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
